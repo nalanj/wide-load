26e82e032eed9594629d6ef2ba0c6836d5c2ea55
Alan Johnson <nalanj@fastmail.com>
2025-01-05 12:09:14 -0500

    Got logfmt logging added

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import { AsyncLocalStorage } from "node:async_hooks";
 // Initialize the service for generating events
 export function WideLoad(args) {
 	const asyncLocalStorage = new AsyncLocalStorage();
-	const serviceName = args.serviceName;
-	const callback = args.callback;
+	const svc = args.serviceName;
+	const callback = args.callback || defaultCallback;
 
 	return {
 		event: (fn) => {
-			return asyncLocalStorage.run({}, () => {
+			return asyncLocalStorage.run({ ts: undefined, svc: undefined }, () => {
 				const result = fn();
 
-				callback({ ...asyncLocalStorage.getStore(), serviceName });
+				const logMsg = asyncLocalStorage.getStore();
+				logMsg.ts = new Date();
+				logMsg.svc = svc;
+
+				callback(logMsg);
 				return result;
 			});
 		},
@@ -20,4 +24,44 @@ export function WideLoad(args) {
 			return asyncLocalStorage.getStore();
 		},
 	};
+}
+
+function defaultCallback(logMsg) {
+	log(logMsg);
+}
+
+function log(logMsg, stream = process.stdout) {
+	stream.write(`${logLine(logMsg)}\n`);
+}
+
+function logLine(logMsg) {
+	return Object.entries(logMsg)
+		.map(([key, value]) => logAttr(key, value))
+		.join(" ");
+}
+
+function logAttr(key, value) {
+	let stringValue = JSON.stringify(value);
+	if (value instanceof Date) {
+		stringValue = value.toISOString();
+	} else {
+		stringValue = value.toString();
+	}
+
+	let out = "";
+	let needsQuotes = false;
+	for (let i = 0; i < stringValue.length; i++) {
+		if (stringValue[i] === '"') {
+			out += '\\"';
+		} else if (stringValue[i] === "\n") {
+			out += "\\n";
+		} else if (stringValue[i] === "\r" || stringValue[i] === "\t") {
+		} else if (stringValue[i] === " ") {
+			needsQuotes = true;
+		} else {
+			out += stringValue[i];
+		}
+	}
+
+	return `${key}=${needsQuotes ? '"' : ""}${out}${needsQuotes ? '"' : ""}`;
 }
---
+++ samples/dump.js
@@ -0,0 +1,19 @@
+import { setTimeout } from "node:timers/promises";
+import { WideLoad } from "../index.js";
+
+const wl = WideLoad({ serviceName: "dump" });
+
+const iterations = process.argv[2] || 1000;
+const delay = process.argv[3] || 0;
+
+console.log(`Running ${iterations} iterations with ${delay}ms delay`);
+
+for (let i = 0; i < iterations; i++) {
+	wl.event(() => {
+		wl.currentMessage.blah = "something\ncool";
+	});
+
+	if (delay) {
+		await setTimeout(delay);
+	}
+}
